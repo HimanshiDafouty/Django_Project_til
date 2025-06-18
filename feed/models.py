0d441e6eea339f1e1402beee1c074ee977394a6e
Himanshi Dafouty <himanshi.dafouty@qvantel.com>
2025-06-18 12:08:29 +0530

    Add HomePage ListView to display latest 30 posts

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+  # Post(models.Model): Creates a database table named Post.
+class Post(models.Model): 
+  
+    # Adds a column text to the table, storing short text up to 240 characters.
+    text = models.CharField(max_length=240)
+    
+    # Adds a column 'date' that automatically updates to the current datetime every time the model is saved.
+    date = models.DateTimeField(auto_now = True)
+    
+    # __str__: This is used to define how your object will appear in the Django admin and shell. It'll show the actual post text instead of something like Post object (1).
+    def __str__(self):
+        return self.text
--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# We use class-based views
+from django.views.generic import ListView
+from .models import Post  # We import the Post model to use it in the view
 
 # Create your views here.
+
+# This view will render a template with the latest 30 posts
+class HomePage(ListView):
+    http_method_names = ["get"]  # Only allows GET requests
+    template_name = "homepage.html"  # Template to be rendered
+    model = Post  # The model this view will use
+    context_object_name = "posts"  # The name used to access the posts in the template
+    queryset = Post.objects.all().order_by('-id')[:30]  # Return the 30 most recent posts
